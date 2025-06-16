2e22507b352fd6007d474509f52c44fa64f5255b
Ferrari dev <152184680+ferraribilly@users.noreply.github.com>
2025-06-16 00:44:22 -0300

    Update build.sh111

--- build.sh
+++ build.sh
@@ -8,5 +8,4 @@ apt install -y ./google-chrome-stable_current_amd64.deb
 CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
 unzip -o chromedriver_linux64.zip
-mv chromedriver /usr/local/bin/
-chmod +x /usr/local/bin/chromedriver
+chmod +x ./chromedriver
