00e51a98a005ad4e46cd60f2159c1cd0dc7d94e5
Ferrari dev <152184680+ferraribilly@users.noreply.github.com>
2025-06-16 00:56:29 -0300

    Update build.sh111111111

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install -y wget unzip curl libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1 libxss1 libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 libnspr4 libxcomposite1 libxdamage1 libxrandr2 xdg-utils libu2f-udev
+set -e
 
+apt-get update
+apt-get install -y wget unzip curl
+
+# Instalar Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
 
+# Instalar ChromeDriver
 CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
-unzip -o chromedriver_linux64.zip
-chmod +x ./chromedriver
+unzip -o chromedriver_linux64.zip -d /usr/local/bin/
+chmod +x /usr/local/bin/chromedriver
+
+# Rodar o servidor com gunicorn
+exec gunicorn bot:app --bind 0.0.0.0:3001
