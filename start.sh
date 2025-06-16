b299333fc4c5ef92e39bba81f0c7b471548a4877
Ferrari dev <152184680+ferraribilly@users.noreply.github.com>
2025-06-16 01:05:26 -0300

    Update start.sh0000000

--- start.sh
+++ start.sh
@@ -1,18 +1,23 @@
-#!/usr/bin/env bash
-set -e
+FROM python:3.11-slim
 
-apt-get update
-apt-get install -y wget unzip curl
+# Instala dependências para o Chrome
+RUN apt-get update && apt-get install -y wget unzip curl gnupg2
 
 # Instalar Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+RUN apt install -y ./google-chrome-stable_current_amd64.deb
 
 # Instalar ChromeDriver
-CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
-wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
-unzip -o chromedriver_linux64.zip -d /usr/local/bin/
-chmod +x /usr/local/bin/chromedriver
+RUN CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE) && \
+    wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip && \
+    unzip chromedriver_linux64.zip -d /usr/local/bin/ && \
+    chmod +x /usr/local/bin/chromedriver
 
-# Rodar o servidor com gunicorn
-exec gunicorn bot:app --bind 0.0.0.0:3001
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["gunicorn", "bot:app", "--bind", "0.0.0.0:3001"]
