d9c33cef73e4d79a706fb911405b07304b493425
Ferrari dev <152184680+ferraribilly@users.noreply.github.com>
2025-06-16 00:27:12 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+apt-get update
+apt-get install -y wget unzip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin/
